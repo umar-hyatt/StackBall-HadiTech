7c1ecae8c4a5291431c66fc4a1997f120a32eaba
Umar <74268119+UmarHyatt-programmer@users.noreply.github.com>
2021-07-29 18:19:27 +0500

    1
    1

---
+++ Assets/Scenes/AdmobController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+ 
+#if UNITY_IOS
+using Unity.Advertisement.IosSupport;
+#endif
+ 
+public class AdmobController : MonoBehaviour
+{
+    private void Start()
+    {
+#if UNITY_IOS
+        if (ATTrackingStatusBinding.GetAuthorizationTrackingStatus() == ATTrackingStatusBinding.AuthorizationTrackingStatus.NOT_DETERMINED)
+        {
+            ATTrackingStatusBinding.RequestAuthorizationTracking();
+        }
+#endif
+        
+        // Other Init
+    }
+    
+}
